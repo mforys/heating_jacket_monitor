db2beced1f4fb8707fd819272112a07be68cc788
Marek Forys <forys@o2.pl>
2021-10-29 18:41:24 +0200

    New file HttpController.swift

---
+++ moniheatme/moniheatme/HttpController.swift
@@ -0,0 +1,20 @@
+//
+//  HttpController.swift
+//  moniheatme
+//
+//  Created by Marek Forys on 29/10/2021.
+//
+
+import Foundation
+
+//Klucz api: DMdBStq23pDENzBGTjh3r4byXRYbqG0w
+//
+//Endpointy:
+//
+//https://b2b.krystian.com.pl/api/heating
+//
+//https://b2b.krystian.com.pl/api/motion
+
+let ApiKey = "DMdBStq23pDENzBGTjh3r4byXRYbqG0w"
+let HeatingHttpEndPoint = "https://b2b.krystian.com.pl/api/heating"
+let MotionHttpEndPoint = "https://b2b.krystian.com.pl/api/motion"
